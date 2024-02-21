8251fb5059f310ac1ec257ee2fd0d877fe382e42
rishiekumaraarachchi <rishieruween199611@gmail.com>
2024-02-21 13:26:33 +0530

    add comments

--- src/main/java/com/example/employeemanagement/Mapper/EmployeeMapper.java
+++ src/main/java/com/example/employeemanagement/Mapper/EmployeeMapper.java
@@ -3,6 +3,7 @@ package com.example.employeemanagement.Mapper;
 import com.example.employeemanagement.dto.EmployeeDto;
 import com.example.employeemanagement.entity.Employee;
 
+// This class is used to convert the Employee entity to EmployeeDto and vice versa.
 public class EmployeeMapper {
     public static EmployeeDto toEmployeeDto(Employee employee) {
         return new EmployeeDto(employee.getId(),
@@ -11,6 +12,7 @@ public class EmployeeMapper {
                 employee.getEmail());
     }
 
+    // This method is used to convert the EmployeeDto to Employee entity.
     public static Employee toEmployee(EmployeeDto employeeDto) {
         return new Employee(employeeDto.getId(),
                 employeeDto.getFirstName(),
